d96a7df876e197aff0352726170b990f94341e2d
Elizabeth Rosenberger <erose25@illinois.edu>
2026-03-24 20:49:04 -0500

    established files and created working notebook

---
+++ scripts/.ipynb_checkpoints/data_cleaning-checkpoint.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def clean_data():
+  print('Cleaning Data...')
+
+if __name__ == "__main__":
+  clean_data()
---
+++ scripts/run_pipeline.py
@@ -0,0 +1,7 @@
+from data_cleaning import clean_data
+
+def main():
+    clean_data()
+
+if __name__ == "__main__":
+    main()
